9d1b57128130cbdc47dfe5f7017e933b941fdbe0
siu <francisco.rodriguez@iesnervion.es>
2024-01-18 11:12:04 +0100

    Agregar archivos de proyecto.

---
+++ EjerciciosUnidad13/Ejercicio01y02/script.js
@@ -0,0 +1,25 @@
+﻿window.onload = inicializaFunciones;
+
+function inicializaFunciones() {
+    document.getElementById("btnSaludarServer").addEventListener("click", saludaServer, false);
+}
+
+function saludaServer() {
+    let request = new XMLHttpRequest();
+    let divMensaje = document.getElementById("mensaje");
+    let indice = 3;
+
+    request.open("GET", "https://crudnervion.azurewebsites.net/api/personas");
+
+    request.onreadystatechange = function () {
+        if (request.readyState < 4) {
+            divMensaje.innerHTML = "Cagando...";
+        } else {
+            if (request.readyState == 4 && request.status == 200) {
+                let response = JSON.parse(request.responseText);
+                divMensaje.innerHTML = response[indice].nombre + " " + response[indice].apellidos;
+            }
+        }
+    };
+    request.send();
+}
---
+++ EjerciciosUnidad13/Ejercicio03/script.js
@@ -0,0 +1,20 @@
+﻿window.onload = tablaPersonas;
+
+function tablaPersonas() {
+    let request = new XMLHttpRequest();
+    let tabla = document.getElementById("tablaPersonas");
+
+    request.open("GET", "https://crudpaco.azurewebsites.net/api/personas");
+
+    request.onreadystatechange = function () {
+        if (request.readyState < 4) {
+            divMensaje.innerHTML = "Cagando...";
+        } else {
+            if (request.readyState == 4 && request.status == 200) {
+                let apiEntera = request.responseText;
+                apiEntera.forEach((persona))
+            }
+        }
+    };
+    request.send();
+}
