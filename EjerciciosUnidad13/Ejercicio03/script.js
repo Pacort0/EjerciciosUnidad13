0f3a4769bda823c5a0b8ccc59a2435cda03b16f0
siu <francisco.rodriguez@iesnervion.es>
2024-01-21 21:10:10 +0100

    no me deja hacer delete illo

--- EjerciciosUnidad13/Ejercicio03/script.js
+++ EjerciciosUnidad13/Ejercicio03/script.js
@@ -1,8 +1,9 @@
-﻿window.onload = tablaPersonas;
+﻿ window.onload = tablaPersonas;
 
 function tablaPersonas() {
     let request = new XMLHttpRequest();
     let tabla = document.getElementById("tablaPersonas");
+    let divMensaje = document.getElementById("mensaje");
 
     request.open("GET", "https://crudpaco.azurewebsites.net/api/personas");
 
@@ -10,9 +11,24 @@ function tablaPersonas() {
         if (request.readyState < 4) {
             divMensaje.innerHTML = "Cagando...";
         } else {
+            divMensaje.innerHTML = "";
             if (request.readyState == 4 && request.status == 200) {
-                let apiEntera = request.responseText;
-                apiEntera.forEach((persona))
+                let apiEntera = JSON.parse(request.responseText);
+                for (i = 0; apiEntera.length; i++) {
+                    var tr = document.createElement("tr");
+                    var tdNombre = document.createElement("td");
+                    var tdApellidos = document.createElement("td");
+                    var tdDepartamento = document.createElement("td");
+
+                    tdNombre.innerHTML = apiEntera[i].nombre;
+                    tdApellidos.innerHTML = apiEntera[i].apellidos;
+                    tdDepartamento.innerHTML = apiEntera[i].idDepartamento;
+
+                    tr.appendChild(tdNombre);
+                    tr.appendChild(tdApellidos);
+                    tr.appendChild(tdDepartamento);
+                    tabla.appendChild(tr);
+                }
             }
         }
     };
---
+++ EjerciciosUnidad13/Ejercicio04/script.js
@@ -0,0 +1,61 @@
+﻿window.onload = iniciaEventos;
+
+function iniciaEventos() {
+    document.getElementById("btnBorrar").addEventListener("click", borraPersona, false)
+}
+
+function borraPersona() {
+    let divMensaje = document.getElementById("mensaje");
+    var personaABorrar = document.getElementById("entryPersona");
+    request.open("GET", "https://crudpaco.azurewebsites.net/api/personas");
+
+    request.onreadystatechange = function () {
+        if (request.readyState < 4) {
+            divMensaje.innerHTML = "Cagando...";
+        } else {
+            if (request.readyState == 4 && request.status == 200) {
+                let apiEntera = JSON.parse(request.responseText);
+                if (apiEntera.includes(apiEntera[personaABorrar].ID = personaABorrar)) {
+                    divMensaje.innerHTML = "Persona borrada con éxito";
+                } else {
+                    divMensaje.innerHTML = "ID no encontrada";
+                }
+            }
+        }
+    };
+    request.send();
+}
+
+
+//window.onload = iniciaEventos;
+
+//function iniciaEventos() {
+//    document.getElementById("btnBorrar").addEventListener("click", borraPersona, false)
+//}
+
+//function borraPersona() {
+//    let divMensaje = document.getElementById("mensaje");
+//    var personaABorrar = document.getElementById("entryPersona");
+//    let request = new XMLHttpRequest();
+
+//    request.open("DELETE", "https://crudpaco.azurewebsites.net/api/personas");
+
+//    request.onreadystatechange = function () {
+//        if (request.readyState == 4) {
+//            if (request.status == 200) {
+//                let apiEntera = JSON.parse(request.responseText);
+//                let personaEncontrada = apiEntera.find(persona => persona.ID == personaABorrar);
+//                if (personaEncontrada) {
+//                    divMensaje.innerHTML = "Persona borrada con éxito";
+//                } else {
+//                    divMensaje.innerHTML = "ID no encontrada";
+//                }
+//            } else {
+//                divMensaje.innerHTML = "Error en la solicitud: " + request.status;
+//            }
+//        } else {
+//            divMensaje.innerHTML = "Cargando...";
+//        }
+//    };
+//    request.send();
+//}
