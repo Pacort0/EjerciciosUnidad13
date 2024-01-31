dca047e4fd7c91d61190a1aeb8aff96f7f3c0808
siu <francisco.rodriguez@iesnervion.es>
2024-01-31 20:54:48 +0100

    Ejercicio06 encaminao

--- EjerciciosUnidad13/Ejercicio06/script.js
+++ EjerciciosUnidad13/Ejercicio06/script.js
@@ -1,63 +1,62 @@
-﻿window.onload = inicializar;
+﻿// JavaScript source code
 
-var divMensaje;
-var listaDepartamentos = [];
-var tabla;
-function inicializar() {
-    divMensaje = document.getElementById("mensaje");
-    tabla = document.getElementById("tablaPersonas");
-    peticionDepartamentos();
+window.onload = inicioPagina;
+
+var listaModelos = [];
+var listaMarcas = [];
+var selectMarcas;
+function inicioPagina() {
+    selectMarcas = document.getElementById("marcas");
+    selectMarcas.addEventListener("change", tablaModelos, false)
+    peticionMarcas();
 }
 const options = {
     method: "GET"
 };
-function peticionDepartamentos() {
-    fetch("https://crudpaco.azurewebsites.net/api/departamentos", options)
+
+function peticionMarcas() {
+    fetch("https://marcasycochespaco.azurewebsites.net/api/marcas", options)
         .then(response => {
             if (response.ok) { //Si la petición es correcta
                 return response.json(); //Parseamos los datos a json
             }
         }).then(data => {
-            listaDepartamentos = data;  //Guardamos los datos en formato json en la tabla
-            peticionPersonas(); //Llamamos a la funcion cuando ya se ha completado el proceso de la lista de departamentos
+            listaMarcas = data;  //Guardamos los datos en formato json en la tabla
+            for (let i = 0; i < listaMarcas.length; i++) {
+                var opt = document.createElement("option");
+                opt.value = listaMarcas[i].id;
+                opt.innerHTML = listaMarcas[i].nombre;
+                selectMarcas.appendChild(opt);
+            }
+            peticionModelos();
         });
 }
-
-function peticionPersonas() {
-    var apiEntera = [];
-    fetch("https://crudpaco.azurewebsites.net/api/personas", options)
+function peticionModelos() {
+    fetch("https://marcasycochespaco.azurewebsites.net/api/modelos", options)
         .then(response => {
-            if (response.ok) {
-                return response.json();
+            if (response.ok) { //Si la petición es correcta
+                return response.json(); //Parseamos los datos a json
             }
-        })
-        .then(data => {
-            apiEntera = data;
-
-            for (let i = 0; i < apiEntera.length; i++) {
-                let contadorDepartamentos = 0;
-                let encontrado = false;
-                var tr = document.createElement("tr");
-                var tdNombre = document.createElement("td");
-                var tdApellidos = document.createElement("td");
-                var tdDepartamento = document.createElement("td");
-
-                tdNombre.innerHTML = apiEntera[i].nombre;
-                tdApellidos.innerHTML = apiEntera[i].apellidos;
-
-                while (encontrado == false && contadorDepartamentos < listaDepartamentos.length) {
-                    if (listaDepartamentos[contadorDepartamentos].idDepartamento == apiEntera[i].idDepartamento) {
-                        tdDepartamento.innerHTML = listaDepartamentos[contadorDepartamentos].nombreDepartamento;
-                        encontrado = true;
-                    }
-                    contadorDepartamentos++;
-                };
-
-                tr.appendChild(tdNombre);
-                tr.appendChild(tdApellidos);
-                tr.appendChild(tdDepartamento);
-                tabla.appendChild(tr);
-            }
+        }).then(data => {
+            listaModelos = data;  //Guardamos los datos en formato json en la tabla
         });
+}
+function tablaModelos() {
+    tablaExistente = document.getElementById("coches")
+    if (tablaExistente) {  //Seleccionamos la tabla (si existe)) 
+        document.body.removeChild(tablaExistente) //Eliminamos toda la tabla
+    }
 
+    var marca = document.getElementById("marcas").value; //Cogemos la marca escogida por el usuario
+    var tabla = document.createElement("table"); //Creamos una tabla
+
+    tabla.createTHead().innerHTML = "Modelos"; //Le damos una cabecera general a la tabla
+    tabla.id = "coches"; //Le damos un id a la tabla para poder seleccionarla más tarde
+
+    listaModelos.forEach((modelo) => {
+        if (modelo.idMarca == marca) {
+            tabla.insertRow().insertCell(0).innerHTML = modelo.nombre;
+        }
+    });
+     document.body.appendChild(tabla); //Añadimos la tabla creada a la página como hija del elemento 'body'
 }
