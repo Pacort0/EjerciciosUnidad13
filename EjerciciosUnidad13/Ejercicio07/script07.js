2e5314fd0fe430facb9cfe3baba5942cd13fff29
siu <francisco.rodriguez@iesnervion.es>
2024-02-03 21:21:54 +0100

    yasta luego sigo

--- EjerciciosUnidad13/Ejercicio07/script07.js
+++ EjerciciosUnidad13/Ejercicio07/script07.js
@@ -11,7 +11,13 @@ class clsPersona {
         this.imageURL = imageURL;
         this.idDepartamento = idDepartamento;
     }
+}
 
+class clsDepartamento {
+    constructor(idDepartamento, nombreDepartamento) {
+        this.idDepartamento = idDepartamento;
+        this.nombreDepartamento = nombreDepartamento;
+    }
 }
 
 var listaPersonasHTML = [];
@@ -24,6 +30,7 @@ var idEdit;
 
 //Cuando se carga la página, se llama a esta función
 function inicioPagina() {
+    creaFormulario();
     listaPersonasHTML = document.getElementById("tablaAlumnos");
     listaDepartamentos = document.getElementById("selectDepartamentos")
     btnEnviar = document.getElementById("btnEnviar").addEventListener("click", ejecutaAccion, false);
@@ -44,6 +51,71 @@ function inicioPagina() {
         localStorage.removeItem('personaEditadaFlag');
     }
 }
+
+function creaFormulario() {
+    //Tomamos el formulario de la pagina HTML
+    var formulario = document.getElementById("idFormulario");
+
+    // Elementos del formulario
+    var elements = [
+        { label: "Nombre", type: "text", id: "inputNombre", placeholder: "Nombre", required: true },
+        { label: "Apellidos", type: "text", id: "inputApellidos", placeholder: "Apellidos", required: true },
+        { label: "Fecha de nacimiento", type: "date", id: "inputFechaNac", placeholder: "Fecha de nacimiento", required: true },
+        { label: "Direccion", type: "text", id: "inputDireccion", placeholder: "Dirección", required: true },
+        { label: "Número de teléfono", type: "tel", id: "inputNumTelf", placeholder: "Núm. de teléfono", required: true },
+        { label: "Foto", type: "text", id: "inputFoto", placeholder: "Url de imagen", required: true },
+    ];
+
+    // Creamos cada label y cada input del form linea a linea
+    elements.forEach(function (elementInfo) {
+        var label = document.createElement("label");
+        label.htmlFor = elementInfo.id;
+        label.textContent = elementInfo.label;
+        formulario.appendChild(label);
+
+        formulario.appendChild(document.createElement("br"));
+
+        var input = document.createElement("input");
+        input.type = elementInfo.type;
+        input.id = elementInfo.id;
+        input.placeholder = elementInfo.placeholder;
+        if (elementInfo.required) {
+            input.required = true;
+        }
+        formulario.appendChild(input);
+
+        formulario.appendChild(document.createElement("br"));
+    });
+
+    // Añadimos el select de departamentos
+    const departmentLabel = document.createElement("label");
+    departmentLabel.textContent = "Escoge un departamento";
+    formulario.appendChild(departmentLabel);
+
+    //Si no pongo un div me da error el botón de submit
+    const departmentDiv = document.createElement("div");
+    departmentDiv.id = "divSelectDepartamentos";
+    const departmentSelect = document.createElement("select");
+    departmentSelect.name = "tablaDepartamentos";
+    departmentSelect.id = "selectDepartamentos";
+
+    //Opción por defecto
+    const defaultOption = document.createElement("option");
+    defaultOption.value = "elige";
+    defaultOption.selected = true;
+    defaultOption.textContent = "Elige un departamento";
+    departmentSelect.appendChild(defaultOption);
+
+    departmentDiv.appendChild(departmentSelect);
+    formulario.appendChild(departmentDiv);
+
+    const btnEnviar = document.createElement("input");
+    btnEnviar.type = "button";
+    btnEnviar.id = "btnEnviar";
+    btnEnviar.value = "Enviar";
+    formulario.appendChild(btnEnviar);
+}
+
 
 const optionGet = {
     method: "GET"
@@ -69,7 +141,7 @@ function peticionDepartamentos() {
 
 //Función que recoge una lista de persona de la api y rellena una tabla de personas con nombre de departamento
 function peticionPersonas() {
-     fetch("https://crudpaco.azurewebsites.net/api/personas", optionGet)
+    fetch("https://crudpaco.azurewebsites.net/api/personas", optionGet)
         .then(response => {
             if (response.ok) {
                 return response.json();
